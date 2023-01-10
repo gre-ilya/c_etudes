894340dd72bee8bb8f25b18767cc9b0b34bbd87a
gre-ilya <ilya.grebenyuk.01@mail.ru>
2023-01-10 13:41:53 +0500

    find substring beginning

--- main.c
+++ main.c
@@ -1,15 +1,30 @@
 #include <stdio.h>
 
-int countSpaces(char* str)
+/*
+ * Finds substring substr in string str and returns a pointer to string
+ * beginning. Returns NULL if substr not found.
+*/
+const char* findSubstring(const char* str, const char* substr)
 {
-	if (*str == '\0')
-		return 0;
-	if (*str == ' ')
-		return countSpaces(++str) + 1;
-	return countSpaces(++str);
+	const char* substr_ptr = substr;
+	int len = 0;
+	while (*str != '\0') {
+		if (*str++ == *substr_ptr) {
+			substr_ptr++;
+			len++;
+		} else {
+			substr_ptr = substr;
+			len = 0;
+		}
+		if (*substr_ptr == '\0') {
+			return str - len;
+		}
+	}
+	return NULL;
 }
 
 int main(int argc, char** argv)
 {
-	printf("%d\n", countSpaces(argv[1]));
+	const char* ptr = findSubstring("one two three", "two");
+	printf("%s\n", ptr);
 }
